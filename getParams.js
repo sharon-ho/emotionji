f6fd5d6f44be6d735fa7e0a3762b13f67f9687b5
Sharon <33813045+sharon-ho@users.noreply.github.com>
2017-12-03 14:15:33 -0500

    javascript changes

--- getParams.js
+++ getParams.js
@@ -2,36 +2,35 @@ var emotes = ["😊", "😠","😐", "😢", "😲", "😱"]
 var text = ["You're feeling positive!", "#$@&%*!", "You don't have an opinion.", "You're not feeling well...", "Woah!", "AAAAHHH!!!"]
 function getParams()
 {			
-			var data = document.URL
-			data = data.slice(data.indexOf("?")+1)
-			var progression = data.slice(0, data.indexOf(")")+1)
-			data = data.slice(data.indexOf(")"), data.length)
-			do {
-				var newUrl
-				if (progression.indexOf("Joy") != -1) {
-					newUrl = 0
-				} else if (progression.indexOf("Neutral") != -1) {
-					newUrl = 2
-				} else if (progression.indexOf("Anger") != -1) {
-					newUrl = 1
-				} else if (progression.indexOf("Surprise") != -1) {
-					newUrl = 4
-				} else if (progression.indexOf("Sad") != -1) {
-					newUrl = 3
-				} else if (progression.indexOf("Fear") != -1) {
-					newUrl = 5
-				}
-				document.getElementById("emote").innerHTML = emotes[newUrl]
-				document.getElementById("text").innerHTML = text[newUrl]
-				var toParse = parseFloat(progression.slice(progression.indexOf("0."), progression.indexOf(".")+3))*100.00
-				var percentage = toParse + "%"
-				document.getElementById("percentage").innerHTML = percentage
-				document.getElementById("percentage").style.width = percentage
-				progression = data.slice(1, data.indexOf(")")+1)
-				data = data.slice(data.indexOf(")"), data.length)
-				sleep(5000);
-			} while (data.indexOf(")]") > 5);
-	
+	var data = document.URL
+	data = data.slice(data.indexOf("?")+1)
+	var progression = data.slice(0, data.indexOf(")")+1)
+	data = data.slice(data.indexOf(")"), data.length)
+	do {
+		var newUrl
+		if (progression.indexOf("Joy") != -1) {
+			newUrl = 0
+		} else if (progression.indexOf("Neutral") != -1) {
+			newUrl = 2
+		} else if (progression.indexOf("Anger") != -1) {
+			newUrl = 1
+		} else if (progression.indexOf("Surprise") != -1) {
+			newUrl = 4
+		} else if (progression.indexOf("Sad") != -1) {
+			newUrl = 3
+		} else if (progression.indexOf("Fear") != -1) {
+			newUrl = 5
+		}
+		document.getElementById("emote").innerHTML = emotes[newUrl]
+		document.getElementById("text").innerHTML = text[newUrl]
+		var toParse = parseFloat(progression.slice(progression.indexOf("0."), progression.indexOf(".")+3))*100.00
+		var percentage = toParse + "%"
+		document.getElementById("percentage").innerHTML = percentage
+		document.getElementById("percentage").style.width = percentage
+		progression = data.slice(1, data.indexOf(")")+1)
+		data = data.slice(data.indexOf(")"), data.length)
+		sleep(5000);
+	} while (data.indexOf(")]") > 5);
 }
 
 function sleep (time) {
